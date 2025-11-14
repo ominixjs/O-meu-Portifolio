fbd267c17ea897d1807edb7d1ccd308986306d9b
Ominixjs <paixaoca2003@gmail.com>
2025-11-14 12:14:23 -0300

    Adicionando estilo as seções

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const container_skills = document.getElementById("skills");
+
+// setInterval(() => {
+//   // Rola a página 50 pixels para esquerda a cada execução
+//     container_skills.scrollBy(50, 0);
+// }, 500);
